702b5a37df66322bf33ed4f40b5fab4e00a4134c
psyriccio <psyriccio@psyzzy.net>
2015-05-26 10:28:15 +0300

    MainRoute skeleton

---
+++ src/main/scala/mokko/uds/MainRoute.scala
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mokko.uds
+
+import com.google.common.io.Files
+import java.io.File
+import spray.http._
+import scala.concurrent.ExecutionContext
+import spray.http.MediaTypes._
+import spray.routing._
+
+trait MainRoute extends Directives with AppLogging {
+  
+  implicit val executionContext = ExecutionContext.global
+
+  val mainRoute: Route = {
+    
+    def path_(value: String) = {
+      (pathPrefix(value) & pathEndOrSingleSlash)
+    }
+    
+    def static(urlPath: String, fromPath: String) = {
+      path(urlPath / Rest) { pathRest =>
+        log.info(s"GET ${urlPath}/${pathRest}")
+        def result(mediaType: MediaType) = {
+          respondWithMediaType(mediaType) {
+            complete {
+              val source = Files.toByteArray(new File(s"${fromPath}/${pathRest}"))
+              val entity = HttpEntity(HttpData(source))
+              entity
+            }
+          }
+        }
+        val suff = pathRest.substring(pathRest.size-3)
+        suff match {
+          case "css" => result(`text/css`)
+          case ".js" => result(`application/javascript`) 
+          case _ => result(`application/octet-stream`)
+        }
+      }
+    }
+    
+    def staticFile(urlPath: String, fileDir: String, fileName: String, mediaType: MediaType) = {
+      path(urlPath) {
+        log.info(s"GET ${urlPath}")
+        respondWithMediaType(mediaType) {
+          complete {
+            val file = new File(new File(fileDir), fileName)
+            if(file.exists()) {
+              val result = HttpEntity(HttpData(Files.toByteArray(file)))
+              file.delete()
+              result  
+            } else {
+              StatusCodes.NotFound
+            }
+          }
+        }
+      }
+    }
+    
+    def staticText(urlPath: String, text: String) = {
+      path(urlPath) {
+        log.info(s"GET ${urlPath}")
+        respondWithMediaType(`text/plain`) {
+          complete {
+            HttpEntity(HttpData(text))
+          }
+        }
+      }
+    }
+  
+    path_("") {
+      get {
+        log.info(s"GET ${requestUri.toString}")
+        complete {
+          s"${buildinfo.buildInfo.name} ${buildinfo.buildInfo.version}"
+        }
+      } 
+    }
+    
+  }
+  
+}
