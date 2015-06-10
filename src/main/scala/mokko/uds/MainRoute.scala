e23bb1888e18665d6c18cdb55f2da26529415154
psyriccio <psyriccio@psyzzy.net>
2015-06-10 13:29:50 +0300

    fix: trim prefix from url

--- src/main/scala/mokko/uds/MainRoute.scala
+++ src/main/scala/mokko/uds/MainRoute.scala
@@ -91,7 +91,7 @@ trait MainRoute extends Directives with AppLogging {
           for(name: Object <- UDSServer.plugins.keySet.toArray) {
             val plugin = UDSServer.plugins.get(name)
             if(pathRest.startsWith(name.asInstanceOf[String])) {
-              result = if(plugin == null) new ServerResponce(StatusCode.NotFound, s"${buildinfo.buildInfo.name} ${buildinfo.buildInfo.version}\n404") else plugin.get(pathRest, clientSession)
+              result = if(plugin == null) new ServerResponce(StatusCode.NotFound, s"${buildinfo.buildInfo.name} ${buildinfo.buildInfo.version}\n404") else plugin.get(pathRest.replaceFirst(name.asInstanceOf[String], ""), clientSession)
             }
           }
           val statusCode = StatusCodes.getForKey(result.getStatusCode).orNull
